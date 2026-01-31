7a0b34886a275ab1716b4a499cce4e3629fbb441
Artem-1-1 <aleksejchukartem@gmail.com>
2026-01-31 12:16:19 +0300

    Fix deploy errors.

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from 'express';
-const app = express();
-import indexRoute from './routes/indexRoute.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+import indexRoute from './routes/indexRoute.js';
 app.use('/', indexRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 export default app;
