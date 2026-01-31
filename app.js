6b5c536aa05eeb43ecc4bbf2c48725a76af43635
Artem-1-1 <aleksejchukartem@gmail.com>
2026-01-31 09:02:34 +0300

    Added connection to db and form page.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import pool from './db/db.js'
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -6,7 +7,10 @@ app.listen(PORT);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 })
+
+export default app;
---
+++ db/db.js
@@ -0,0 +1,23 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error with database connection:', err);
+    return;
+  }
+  console.log('Connection to database is success!');
+  release();
+});
+
+export default pool;
